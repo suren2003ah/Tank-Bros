5708d3a2f93413cc39d3667ba26bc7dccd3dc051
Suren Hakobyan <suren_hakobyan@aybschool.am>
2020-07-18 17:09:41 +0400

    player disappears when hit

---
+++ Assets/killPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killPlayer : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Player")
+        {
+            Destroy(collisionInfo.collider.gameObject);
+        }
+    }
+}
