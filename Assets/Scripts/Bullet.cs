5061cdacd707e46eb6acffb001e2de5231e6a462
MintSoup <aregak2005@gmail.com>
2020-07-21 19:07:48 +0400

    Added sounds with cringe bounce

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,30 +6,46 @@ public class Bullet : MonoBehaviour {
 	public GameObject ParticleDeath;
 	public Player player;
 	private int ticks = 0;
+
+	private AudioSource audioSrc;
+
+	public AudioClip shoot;
+	public AudioClip hit;
+
+
 	void OnCollisionEnter(Collision collisionInfo) {
 		if (collisionInfo.collider.tag == "Player") {
-			if (ticks > (0.1f/Time.deltaTime) || collisionInfo.collider.gameObject != player.gameObject) {
+			if (ticks > (0.1f / Time.deltaTime) || collisionInfo.collider.gameObject != player.gameObject) {
 				collisionInfo.collider.gameObject.SetActive(false);
 				Instantiate(ParticleDeath, transform.position, Quaternion.identity);
 				CameraShaker.Instance.ShakeOnce(13f, 5f, 0.3f, 2f);
 				Destroy(gameObject);
+				SoundManager.PlayDie();
 			}
 		}
-		if (collisionInfo.collider.tag == "Wall") {
+		else if (collisionInfo.collider.tag == "Wall") {
 			CameraShaker.Instance.ShakeOnce(1f, 2f, 0.1f, .5f);
 			if (ticks <= 3) {
 				player.gameObject.SetActive(false);
 				Instantiate(ParticleDeath, transform.position, Quaternion.identity);
 				CameraShaker.Instance.ShakeOnce(13f, 5f, 0.3f, 2f);
 				Destroy(gameObject);
+				SoundManager.PlayDie();
 			}
+			SoundManager.PlayHit();
 		}
+		else if (collisionInfo.collider.tag == "Bullet") {
+			SoundManager.PlayHit();
+		}
+
 		if (deathTime <= Time.time) {
 			Destroy(gameObject);
 		}
 	}
 	void Start() {
 		deathTime = Time.time + lifeTime;
+		audioSrc = GetComponent<AudioSource>();
+		SoundManager.PlayShoot();
 	}
 	void Update() {
 		ticks++;
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	private AudioSource src;
+
+	public AudioClip shoot;
+	public AudioClip hit;
+	public AudioClip die;
+
+	static SoundManager mgr;
+
+	private void RandomPitch() {
+		src.pitch = Random.Range(0.75f, 1.25f);
+	}
+
+	void Start() {
+		src = GetComponent<AudioSource>();
+		mgr = this;
+	}
+
+	public static void PlayHit() {
+		mgr.RandomPitch();
+		mgr.src.PlayOneShot(mgr.hit);
+	}
+	public static void PlayShoot() {
+		mgr.RandomPitch();
+		mgr.src.PlayOneShot(mgr.shoot);
+	}
+	public static void PlayDie() {
+		mgr.RandomPitch();
+		mgr.src.PlayOneShot(mgr.die);
+	}
+
+}
