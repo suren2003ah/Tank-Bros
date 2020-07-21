979b4052cbf1974a386cb23927c1a4c913e2cc1f
MintSoup <aregak2005@gmail.com>
2020-07-21 20:10:12 +0400

    PlayerDeath particles now selfdestruct to avoid memory hogging

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+	// Start is called before the first frame update
+	public float lifetime;
+	private float deathtime;
+	void Start() {
+		deathtime = Time.time + lifetime;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Time.time >= deathtime) Destroy(gameObject);
+	}
+}
