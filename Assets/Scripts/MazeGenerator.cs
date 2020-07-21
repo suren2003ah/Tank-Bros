028ff8e6c504e29335ad51f752c44c72757f59ef
MintSoup <aregak2005@gmail.com>
2020-07-21 17:23:30 +0400

    Maze now correctly generates at random sizes, doesn't reload scene every time + more or less fixed bullet bug

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
-using UnityEngine.SceneManagement;
 
 public class MazeGenerator : MonoBehaviour {
 
@@ -12,44 +11,69 @@ public class MazeGenerator : MonoBehaviour {
 	public Transform Player3;
 	private int length = 1;
 	public float Walldistance;
-	public int GridX, GridY;
+	private int GridX, GridY;
 	public float balanceCoefficient;
 	private bool gameOver = false;
 	private Vector2 player1;
 	private Vector2 player2;
 	private Vector2 player3;
 	void Start() {
-		for (int u = 0; u < length; u++) {
-			player1 = new Vector2(Random.Range(1, GridX + 1), Random.Range(1, GridY + 1));
-			player2 = new Vector2(Random.Range(1, GridX + 1), Random.Range(1, GridY + 1));
-			player3 = new Vector2(Random.Range(1, GridX + 1), Random.Range(1, GridY + 1));
-			if (player1.x == player2.x || player1.x == player3.x || player3.x == player2.x || player1.y == player2.y || player1.y == player3.y || player3.y == player2.y) {
-				length++;
-			}
-		}
 		GenerateMaze();
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 	}
 
 	void GenerateMaze() {
+		//Remove all walls from previous game
+		foreach (GameObject wall in GameObject.FindGameObjectsWithTag("Wall")) {
+			Destroy(wall);
+			wall.SetActive(false);
+		}
+		//Respawn all players
+		if (Player1)
+			Player1.gameObject.SetActive(true);
+		if (Player2)
+			Player2.gameObject.SetActive(true);
+		if (Player3)
+			Player3.gameObject.SetActive(true);
+
+		//Remove all bullets from last game
+
+		foreach (GameObject bullet in GameObject.FindGameObjectsWithTag("Bullet")) {
+			Destroy(bullet);
+			bullet.SetActive(false);
+		}
+		gameOver = false;
+
+		GridX = Random.Range(3, 12); 
+		GridY = Random.Range(3, 6);
+		for (int u = 0; u < length; u++) {
+			player1 = new Vector2(Random.Range(1, GridX + 1), Random.Range(1, GridY + 1));
+			player2 = new Vector2(Random.Range(1, GridX + 1), Random.Range(1, GridY + 1));
+			player3 = new Vector2(Random.Range(1, GridX + 1), Random.Range(1, GridY + 1));
+			//	if (player1.x == player2.x || player1.x == player3.x || player3.x == player2.x || player1.y == player2.y || player1.y == player3.y || player3.y == player2.y) {
+			//	length++;
+			//	}
+		}
+
+		Vector3 bottomLeft = transform.position - new Vector3(GridX * Walldistance, 0, GridY * Walldistance) * 0.5f;
 		for (int x = 1; x < GridX + 1; x++) {
 			for (int y = 0; y < GridY + 1; y++) {
-				Instantiate(Wall, transform.position + new Vector3(x * Walldistance, 0, y * Walldistance), Quaternion.identity);
+				Instantiate(Wall, bottomLeft + new Vector3(x * Walldistance, 0, y * Walldistance), Quaternion.identity);
 			}
 		}
 		Quaternion rotated = Quaternion.Euler(0, 90, 0);
 
 		for (int x = 0; x < GridX + 1; x++) {
 			for (int y = 1; y < GridY + 1; y++) {
-				Instantiate(Wall, transform.position + new Vector3((x + 0.5f) * Walldistance, 0, (y - 0.5f) * Walldistance), rotated);
+				Instantiate(Wall, bottomLeft + new Vector3((x + 0.5f) * Walldistance, 0, (y - 0.5f) * Walldistance), rotated);
 			}
 		}
 
 
 		for (int x = 1; x < GridX + 1; x++) {
 			for (int y = 1; y < GridY + 1; y++) {
-				Vector3 center = transform.position + new Vector3((x) * Walldistance, 2.2f, (y - 0.5f) * Walldistance);
+				Vector3 center = bottomLeft + new Vector3((x) * Walldistance, 2.2f, (y - 0.5f) * Walldistance);
 				if (Player1 && x == player1.x && y == player1.y) {
 					Player1.position = center;
 					Player1.rotation = Quaternion.Euler(0, Random.Range(0, 360f), 0);
@@ -94,7 +118,7 @@ public class MazeGenerator : MonoBehaviour {
 	}
 
 	IEnumerator Reload() {
-		yield return new WaitForSeconds(3f);
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		yield return new WaitForSeconds(2f);
+		GenerateMaze();
 	}
 }
