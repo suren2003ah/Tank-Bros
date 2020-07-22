f8a01bebf5c15686e88635628c1eb3ba146a0e98
MintSoup <aregak2005@gmail.com>
2020-07-22 17:33:07 +0400

    ACTUALLY fixed bulletbug

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,17 +5,18 @@ public class Bullet : MonoBehaviour {
 	private float deathTime;
 	public GameObject ParticleDeath;
 	public Player player;
-	private int ticks = 0;
 
 	private AudioSource audioSrc;
 
 	public AudioClip shoot;
 	public AudioClip hit;
 
+	private float alive;
+
 
 	void OnCollisionEnter(Collision collisionInfo) {
 		if (collisionInfo.collider.tag == "Player") {
-			if (ticks > (0.1f / Time.deltaTime) || collisionInfo.collider.gameObject != player.gameObject) {
+			if (alive >= 0.1f || collisionInfo.collider.gameObject != player.gameObject) {
 				collisionInfo.collider.gameObject.SetActive(false);
 				Instantiate(ParticleDeath, transform.position, Quaternion.identity);
 				CameraShaker.Instance.ShakeOnce(13f, 5f, 0.3f, 2f);
@@ -25,7 +26,7 @@ public class Bullet : MonoBehaviour {
 		}
 		else if (collisionInfo.collider.tag == "Wall") {
 			CameraShaker.Instance.ShakeOnce(1f, 2f, 0.1f, .5f);
-			if (ticks <= 3) {
+			if (Vector3.Distance(transform.position, player.transform.position) <= 7f) {
 				player.gameObject.SetActive(false);
 				Instantiate(ParticleDeath, transform.position, Quaternion.identity);
 				CameraShaker.Instance.ShakeOnce(13f, 5f, 0.3f, 2f);
@@ -48,6 +49,6 @@ public class Bullet : MonoBehaviour {
 		SoundManager.PlayShoot();
 	}
 	void Update() {
-		ticks++;
+		alive += Time.deltaTime;
 	}
 }
