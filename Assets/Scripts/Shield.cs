8645a17e93048763d46dd877b7ab266596a306ff
Suren Hakobyan <suren_hakobyan@aybschool.am>
2020-07-26 16:39:19 +0400

    shield bug fix

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -4,14 +4,17 @@ public class Shield : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        // other.gameObject.GetComponent<Bullet>().player != GetComponentInParent<Player>() &&
+        // other.gameObject.GetComponent<Bullet>().player != GetComponentInParent<Player>() && 
         if (other.tag == "Bullet")
         {
-            if (other.gameObject.GetComponent<Bullet>().alive >= 0.1f)
+            /*float distance1 = Vector3.Distance(other.gameObject.transform.position, transform.position);
+            float distance2 = Vector3.Distance(other.gameObject.transform.position + other.gameObject.GetComponent<Rigidbody>().velocity, transform.position);*/
+            if (other.gameObject.GetComponent<Bullet>().alive >= 0.05f)
             {
                 Destroy(other.gameObject);
                 gameObject.SetActive(false);
-            }
+            }   
         }
+
     }
 }
